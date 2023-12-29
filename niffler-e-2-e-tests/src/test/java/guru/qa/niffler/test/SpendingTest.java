ffe0055633123938232fe08febec2708240314bd
Darya Chibisova <darya.chibisova@raiffeisen.ru>
2023-12-29 14:33:52 +0300

    fix

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingTest.java
@@ -2,21 +2,14 @@ package guru.qa.niffler.test;
 
 import com.codeborne.selenide.Configuration;
 import guru.qa.niffler.jupiter.GenerateCategory;
-import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.jupiter.DisabledByIssue;
 import guru.qa.niffler.jupiter.GenerateSpend;
 import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
 import guru.qa.niffler.po.LoginPage;
 import guru.qa.niffler.po.MainPage;
-import io.qameta.allure.Allure;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.CollectionCondition.size;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
 
 public class SpendingTest extends BaseWebTest {
     private final LoginPage loginPage = new LoginPage();
@@ -44,24 +37,4 @@ public class SpendingTest extends BaseWebTest {
                 .clickDeleteSelectedButton()
                 .checkSpendingElementDisappear();
     }
-
-  @DisabledByIssue("74")
-  @Test
-  void spendingShouldBeDeletedByButtonDeleteSpending(SpendJson spend) {
-    $(".spendings-table tbody")
-        .$$("tr")
-        .find(text(spend.description()))
-        .$$("td")
-        .first()
-        .click();
-
-    Allure.step("Delete spending", () -> $(byText("Delete selected"))
-        .click());
-
-    Allure.step("Check that spending was deleted", () -> {
-      $(".spendings-table tbody")
-          .$$("tr")
-          .shouldHave(size(0));
-    });
-  }
 }
