2d6f2e8d27de9d1bc5df7238523ad669cee23d9e
dtuchs <dtuchs@gmail.com>
2024-01-29 21:36:30 +0300

    Spring-JDBC (vol 4)
    (cherry picked from commit 0722b8c785e7e5ee653f69fd1bf73d1f08e39d3d)

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingTest.java
@@ -2,8 +2,8 @@ package guru.qa.niffler.test;
 
 import com.codeborne.selenide.Configuration;
 import guru.qa.niffler.jupiter.GenerateCategory;
-import guru.qa.niffler.jupiter.DisabledByIssue;
-import guru.qa.niffler.jupiter.GenerateSpend;
+import guru.qa.niffler.jupiter.annotation.DisabledByIssue;
+import guru.qa.niffler.jupiter.annotation.GenerateSpend;
 import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
 import guru.qa.niffler.po.LoginPage;
